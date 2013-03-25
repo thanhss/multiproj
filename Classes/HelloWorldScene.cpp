61ddc35fadfce0f90b72e785ac415314d82198ad
Tyrice Clark <tyricec@new-host-2.home>
2013-03-25 14:28:53 -0700

    Changes to make it compatible with iOS and Mac

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -97,7 +97,7 @@ void HelloWorld::menuCloseCallback(CCObject* pSender)
 {
     CCDirector::sharedDirector()->end();
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS )
     exit(0);
 #endif
 }
