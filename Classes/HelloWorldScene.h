1efe194a8dc2d801cb4148c0012eb49f6fc9ccb8
Thanh Ho <thanhsosa@gmail.com>
2013-03-25 13:33:32 -0700

    add touch event: player will move to touch point

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -11,12 +11,18 @@ public:
 
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::CCScene* scene();
+
+	cocos2d::CCSprite* player;
     
     // a selector callback
     void menuCloseCallback(CCObject* pSender);
     
     // implement the "static node()" method manually
     CREATE_FUNC(HelloWorld);
+
+	// touch callback
+	void ccTouchesEnded(cocos2d::CCSet* pTouches, cocos2d::CCEvent* pEvent);
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
